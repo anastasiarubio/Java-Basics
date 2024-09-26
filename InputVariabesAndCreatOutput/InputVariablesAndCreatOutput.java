a01ff6b6c03e7531f2bd8c3d8535cc11f58e8175
Anastasia <anastasiarubio@outlook.com>
2024-09-26 14:03:59 +0100

    Second exersise

---
+++ InputVariabesAndCreatOutput/InputVariablesAndCreatOutput.java
@@ -0,0 +1,15 @@
+package InputVariabesAndCreatOutput;
+
+import javax.swing.JOptionPane;
+
+public class InputVariablesAndCreatOutput {
+    
+    public static void main(String[] args) {
+        int x=Integer.parseInt(JOptionPane.showInputDialog(null,"Add your age"));
+		int y=Integer.parseInt(JOptionPane.showInputDialog(null,"What is current year"));
+		int ans=y-x;
+		//print
+		JOptionPane.showMessageDialog (null,ans);
+    
+}
+}
---
+++ InputVariabesAndCreatOutput/readme.md
@@ -0,0 +1,3 @@
+Application that takes in two variables from a user; their age and the current year.
+
+The application should  output the year that they were born.
